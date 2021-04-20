cc7788c5199ccb4636fe678d955738be2e5aec20
zhoutian <961668232@qq.com>
2021-04-20 17:48:23 +0800

    增加样式

--- site/pages/Component.tsx
+++ site/pages/Component.tsx
@@ -4,12 +4,13 @@ export default function ComponentPage({ markdown }: any) {
   console.log(markdown.demo.content, 888)
   const Demo = markdown.Demo;
   return (
-    <div className="markdown-container markdown ">
-      <div className="demo">
+    <div className="markdown-container">
+      <div className="demon markdown">
         <h3>示例：</h3>
         <Demo />
       </div>
       <div
+        className="markdown"
         dangerouslySetInnerHTML={{
           __html: markdown.document.content,
         }}
